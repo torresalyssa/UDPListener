bf1d7b0f49a182b85492c6cdd23da99ceaf7aed5
atorres <torres.lyssa@gmail.com>
2016-05-31 17:36:14 -0700

    adding audio multicast listener

--- app/src/main/java/com/ourglass/alyssa/udplistener/MainActivity.java
+++ app/src/main/java/com/ourglass/alyssa/udplistener/MainActivity.java
@@ -1,19 +1,207 @@
 package com.ourglass.alyssa.udplistener;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by atorres on 5/10/16.
  */
 public class MainActivity extends Activity {
+    static final String LOG_TAG = "AUDIO_RECEIVER";
+
+    static final String INET_ADDR = "224.0.0.3";
+    static final int AUDIO_PORT = 8888;
+    static final int SAMPLE_RATE = 44100;
+    static final int SAMPLE_INTERVAL = 20; // milliseconds
+    static final int SAMPLE_SIZE = 2; // bytes per sample
+    static final int BUF_SIZE = SAMPLE_INTERVAL*SAMPLE_INTERVAL*SAMPLE_SIZE*2;
+
+    private AudioTrack mTrack = null;
+    private Boolean mReceiving = false;
+    private DatagramSocket socket = null;
+    private MediaCodec mDecoder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService(new Intent(getBaseContext(), UDPListenerService.class));
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiManager.MulticastLock multicastLock = wifi.createMulticastLock("main");
+        multicastLock.acquire();
+
+        Button btnRecv = (Button) findViewById(R.id.btnRecv);
+        btnRecv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                receiveAudio();
+            }
+        });
+
+        Button btnPause = (Button) findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTrack != null) {
+                    mTrack.pause();
+                }
+            }
+        });
+
+        Button btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTrack != null) {
+                    mTrack.play();
+                }
+            }
+        });
+
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mReceiving = false;
+                socket.close();
+                mDecoder.stop();
+            }
+        });
+
+        //startService(new Intent(getBaseContext(), UDPListenerService.class));
+    }
+
+    public void receiveAudio() {
+        mReceiving = true;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                Log.d(LOG_TAG, "start receive thread, thread id: "
+                        + Thread.currentThread().getId());
+
+                mTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                        SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT, BUF_SIZE,
+                        AudioTrack.MODE_STREAM);
+                mTrack.play();
+
+                try {
+                    InetAddress address = InetAddress.getByName(INET_ADDR);
+                    MulticastSocket mcastSocket = new MulticastSocket(AUDIO_PORT);
+                    mcastSocket.joinGroup(address);
+
+                    byte[] receiveBuf = new byte[BUF_SIZE];
+
+                    setDecoder();
+                    mDecoder.start();
+
+                    while (mReceiving) {
+                        DatagramPacket packet = new DatagramPacket(receiveBuf, receiveBuf.length);
+                        mcastSocket.receive(packet);
+                        Log.d(LOG_TAG, "received packet: " + packet.getLength());
+
+                        byte[] data = new byte[packet.getLength()];
+                        System.arraycopy(packet.getData(), packet.getOffset(), data, 0, packet.getLength());
+
+                        ByteBuffer[] inputBuffers = null, outputBuffers = null;
+                        ByteBuffer inputBuffer = null, outputBuffer = null;
+
+                        if (Build.VERSION.SDK_INT < 21) {
+                            inputBuffers = mDecoder.getInputBuffers();
+                            outputBuffers = mDecoder.getOutputBuffers();
+                        }
+
+                        int inputBufferIdx = mDecoder.dequeueInputBuffer(-1);
+
+                        if (inputBufferIdx >= 0) {
+
+                            if (Build.VERSION.SDK_INT >= 21) {
+                                inputBuffer = mDecoder.getInputBuffer(inputBufferIdx);
+                            } else if (inputBuffers != null) {
+                                inputBuffer = inputBuffers[inputBufferIdx];
+                            }
+
+                            if (inputBuffer != null) {
+                                inputBuffer.clear();
+                                inputBuffer.put(data);
+                                mDecoder.queueInputBuffer(inputBufferIdx, 0, data.length, 0, 0);
+                            }
+                        }
+
+                        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+                        int outputBufferIdx = mDecoder.dequeueOutputBuffer(bufferInfo, 0);
+
+                        while (outputBufferIdx >= 0) {
+
+                            if (Build.VERSION.SDK_INT >= 21) {
+                                outputBuffer = mDecoder.getOutputBuffer(outputBufferIdx);
+                            } else if (outputBuffers != null) {
+                                outputBuffer = outputBuffers[outputBufferIdx];
+                            }
+
+                            if (outputBuffer != null) {
+                                outputBuffer.position(bufferInfo.offset);
+                                outputBuffer.limit(bufferInfo.offset + bufferInfo.size);
+
+                                byte[] outData = new byte[bufferInfo.size];
+                                outputBuffer.get(outData);
+
+                                mTrack.write(outData, 0, outData.length);
+                            }
+
+                            mDecoder.releaseOutputBuffer(outputBufferIdx, false);
+                            outputBufferIdx = mDecoder.dequeueOutputBuffer(bufferInfo, 0);
+                        }
+                    }
+
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Exception: " + e);
+                    e.printStackTrace();
+
+                } finally {
+                    mDecoder.stop();
+                    mTrack.stop();
+
+                    if (socket != null) {
+                        socket.close();
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private void setDecoder() throws IOException {
+        mDecoder = MediaCodec.createDecoderByType("audio/mp4a-latm");
+        MediaFormat format = new MediaFormat();
+        format.setString(MediaFormat.KEY_MIME, "audio/mp4a-latm");
+        format.setInteger(MediaFormat.KEY_BIT_RATE, 64 * 1024);
+        format.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 1);
+        format.setInteger(MediaFormat.KEY_SAMPLE_RATE, SAMPLE_RATE);
+        format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectHE);
+        mDecoder.configure(format, null, null, 0);
     }
 }
--- app/src/main/java/com/ourglass/alyssa/udplistener/UDPListenerService.java
+++ app/src/main/java/com/ourglass/alyssa/udplistener/UDPListenerService.java
@@ -5,50 +5,74 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  * Created by atorres on 5/10/16.
  */
 public class UDPListenerService extends Service {
-    private static int port = 9090;
-    private DatagramSocket socket;
+
+    static final String TAG = "UDP_LISTENER";
+
+    String MCAST_GROUP = "224.1.1.1";
+    int MCAST_PORT = 5007;
+
+    MulticastSocket mSocket = null;
+    InetAddress mAddr = null;
+
+    //private static int port = 9090;
+    //private DatagramSocket socket;
     private Boolean listening = false;
 
-    private WifiManager.MulticastLock lock;
+    private WifiManager.MulticastLock multicastLock;
+    //private PowerManager.WakeLock wakeLock;
 
-    private void getUdpPacket() {
-        byte[] recvBuf = new byte[576];
+    private void getMulticastPacket() {
+        byte[] recvBuf = new byte[256];
 
-        if (socket == null || socket.isClosed()) {
+        if (mAddr == null) {
             try {
-                socket = new DatagramSocket(port);
-                socket.setBroadcast(true);
-            } catch (SocketException e) {
-                Log.e("UDP_SOCK", e.getLocalizedMessage());
+                mAddr = InetAddress.getByName(MCAST_GROUP);
+            } catch (UnknownHostException e) {
+                Log.e(TAG, e.getLocalizedMessage());
+                return;
+            }
+        }
+
+        if (mSocket == null || mSocket.isClosed()) {
+            try {
+                mSocket = new MulticastSocket(MCAST_PORT);
+                mSocket.joinGroup(mAddr);
+            } catch (IOException e) {
+                Log.e(TAG, e.getLocalizedMessage());
                 return;
             }
         }
 
         DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-        Log.i("UDP_LISTENER", "Waiting for UDP broadcast...");
+        Log.i(TAG, "Waiting for UDP broadcast...");
 
         try {
-            socket.receive(packet);
+            mSocket.receive(packet);
         } catch (IOException e) {
-            Log.e("UDP_SOCK", e.getLocalizedMessage());
+            Log.e(TAG, e.getLocalizedMessage());
             return;
         }
 
         String senderIp = packet.getAddress().getHostAddress();
         String message = new String(packet.getData()).trim();
 
-        Log.i("UDP_LISTENER", "Got UDP broadcast from " + senderIp + "\n" + message);
+        Log.i(TAG, "Got UDP broadcast from " + senderIp + "\n" + message);
     }
 
     private void startListening() {
@@ -57,10 +81,11 @@ public class UDPListenerService extends Service {
         Thread UDPBeaconThread = new Thread(new Runnable() {
             public void run() {
                 while (listening) {
-                    Log.i("UDP_LISTENER", "mulitcast lock held: " + lock.isHeld());
-                    getUdpPacket();
+                    getMulticastPacket();
                 }
-                socket.close();
+                if (mSocket != null) {
+                    mSocket.close();
+                }
             }
         });
 
@@ -68,8 +93,8 @@ public class UDPListenerService extends Service {
     }
 
     private void stopListening() {
-        Log.i("UDP_LISTENER", "Stopping listening.");
-        socket.close();
+        Log.i(TAG, "Stopping listening.");
+        mSocket.close();
         listening = false;
     }
 
@@ -77,7 +102,7 @@ public class UDPListenerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("UDP_LISTENER", "onStartCommand");
+        Log.i(TAG, "onStartCommand");
         return Service.START_NOT_STICKY;
     }
 
@@ -90,13 +115,18 @@ public class UDPListenerService extends Service {
     public void onCreate() {
         super.onCreate();
         listening = true;
-        Log.i("UDP_LISTENER", "Service starting.");
+        Log.i(TAG, "Service starting.");
 
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        lock = wifi.createMulticastLock("main");
-        lock.acquire();
+        multicastLock = wifi.createMulticastLock(TAG);
+        multicastLock.acquire();
 
-        Log.i("UDP_LISTENER", "mulitcast lock held: " + lock.isHeld());
+        //PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        //wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+        //wakeLock.acquire();
+
+        Log.i(TAG, "mulitcast lock held: " + multicastLock.isHeld());
+        //Log.i(TAG, "wake lock held: " + wakeLock.isHeld());
 
         startListening();
     }
@@ -104,8 +134,9 @@ public class UDPListenerService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i("UDP_LISTENER", "Service stopping.");
-        lock.release();
+        Log.i(TAG, "Service stopping.");
+        multicastLock.release();
+        //wakeLock.release();
         stopListening();
     }
 }
